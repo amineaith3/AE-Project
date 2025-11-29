3e436e7f6ef66da52b179849feb1aa252df88376
Saadiaboussiar <boussiarsaadia@gmail.com>
2025-11-29 21:08:15 +0100

    Altered The tables to meet the Business Logic

---
+++ tables/ALTER_TABLES.sql
@@ -0,0 +1,25 @@
+ALTER TABLE Passengers
+ADD COLUMN Age NUMBER NOT NULL;
+
+ALTER TABLE Aircrafts
+DROP COLUMN CurrentCapacity;
+
+ALTER TABLE Reservations
+ADD COLUMN Guardian_id NUMBER;
+
+ALTER TABLE Flights
+ADD COLUMN CurrentCapacity NUMBER NOT NULL;
+
+
+ALTER TABLE Flights ADD CONSTRAINT chk_flight_state 
+CHECK (state IN ('Scheduled', 'In Service', 'Cancelled', 'Full'));
+
+
+ALTER TABLE Reservations ADD CONSTRAINT chk_reservation_state 
+CHECK (State IN ('Pending', 'Confirmed', 'Cancelled', 'Boarded'));
+
+
+ALTER TABLE Maintenance ADD CONSTRAINT chk_maintenance_state 
+CHECK (State IN ('Scheduled', 'In Progress', 'Completed'));
+
+
