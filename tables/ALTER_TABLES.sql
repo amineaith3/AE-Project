72be5b42783693e579d394f5aba73af9302ee727
Saadiaboussiar <boussiarsaadia@gmail.com>
2025-11-29 21:08:59 +0100

    Altered tables to meet the Business Logic

--- tables/ALTER_TABLES.sql
+++ tables/ALTER_TABLES.sql
@@ -10,6 +10,13 @@ ADD COLUMN Guardian_id NUMBER;
 ALTER TABLE Flights
 ADD COLUMN CurrentCapacity NUMBER NOT NULL;
 
+ALTER TABLE Flights
+ADD arrival_time DATE NOT NULL;
+
+ALTER TABLE Aircrafts ADD CONSTRAINT chk_aircrafts_state 
+CHECK (State IN ('Ready', 'Flying', 'Turnaround' ,'Maintenance', 'Out of Service'));
+--Turnaround = Quick checks, refueling, cleaning (15-60 mins)
+
 
 ALTER TABLE Flights ADD CONSTRAINT chk_flight_state 
 CHECK (state IN ('Scheduled', 'In Service', 'Cancelled', 'Full'));
