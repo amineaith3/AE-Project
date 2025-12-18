859ae4ed1a7255cf6a41a98ca4eda0dd816df1a4
AsmaAoub <136008375+AsmaAoub@users.noreply.github.com>
2025-12-18 12:23:15 +0100

    Update SQL triggers for reservations management

---
+++ Aeoropor fastapi/plsql/triggers/riggers_reservations.sql
@@ -0,0 +1,39 @@
+
+CREATE OR REPLACE TRIGGER before_insert_reservation
+BEFORE INSERT ON RESERVATIONS
+FOR EACH ROW
+DECLARE
+    v_taken NUMBER;
+BEGIN
+    SELECT COUNT(*) INTO v_taken
+    FROM RESERVATIONS
+    WHERE VolNum = :NEW.VolNum AND SeatCode = :NEW.SeatCode;
+
+    IF v_taken > 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Le siège est déjà pris.');
+    END IF;
+END;
+/
+
+
+CREATE OR REPLACE TRIGGER after_insert_reservation
+AFTER INSERT ON RESERVATIONS
+FOR EACH ROW
+BEGIN
+    UPDATE FLIGHTS
+    SET CurrentCapacity = CurrentCapacity + 1
+    WHERE VolNum = :NEW.VolNum;
+END;
+/
+
+
+CREATE OR REPLACE TRIGGER after_delete_reservation
+AFTER DELETE ON RESERVATIONS
+FOR EACH ROW
+BEGIN
+    UPDATE FLIGHTS
+    SET CurrentCapacity = CurrentCapacity - 1
+    WHERE VolNum = :OLD.VolNum
+      AND CurrentCapacity > 0;
+END;
+/
