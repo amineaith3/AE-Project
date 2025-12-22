454e00872801ef8de614eebaa669356f84c3a528
AsmaAoub <136008375+AsmaAoub@users.noreply.github.com>
2025-12-22 12:56:36 +0100

    Remove unnecessary comment in reservations.py
    Removed a commented line about importing schemas.

--- Aeoropor fastapi/app/routes/reservations.py
+++ Aeoropor fastapi/app/routes/reservations.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
 from app.database import get_db
-# On importe les différents schémas pour les utiliser aux bons endroits
 from app.schemas import ReservationCreate, ReservationResponse 
 from app.crud import reservations as crud_res
 
