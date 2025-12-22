b9135267f7b1a4ea575be3ecca58983a0e5ac334
AsmaAoub <136008375+AsmaAoub@users.noreply.github.com>
2025-12-22 12:31:04 +0100

    Refactor maintenance triggers for consistency and clarity

--- Aeoropor fastapi/plsql/triggers/triggers_maintenance.sql
+++ Aeoropor fastapi/plsql/triggers/triggers_maintenance.sql
@@ -1,51 +1,59 @@
-CREATE OR REPLACE TRIGGER before_insert_maintenance
-BEFORE INSERT ON MAINTENANCE
-FOR EACH ROW
-DECLARE
-    v_state VARCHAR2(50);
-BEGIN
-    SELECT State INTO v_state
-    FROM AIRCRAFTS
-    WHERE AvionID = :NEW.AvionID;
-
-    IF v_state = 'Maintenance' THEN
-        RAISE_APPLICATION_ERROR(-20001, 'Avion déjà en maintenance.');
-    END IF;
-END;
-/
-
-CREATE OR REPLACE TRIGGER after_insert_maintenance
+CREATE OR REPLACE TRIGGER trg_after_insert_maintenance
 AFTER INSERT ON MAINTENANCE
 FOR EACH ROW
 BEGIN
+    
     IF TRUNC(:NEW.OperationDate) = TRUNC(SYSDATE) THEN
-        UPDATE AIRCRAFTS
-        SET State = 'Maintenance'
-        WHERE AvionID = :NEW.AvionID;
+        UPDATE Aircrafts 
+        SET State = 'Maintenance' 
+        WHERE Avion_id = :NEW.Avion_id;
     END IF;
 
+    -- Insertion dans les logs (Assurez-vous que la table LOGS existe)
     INSERT INTO LOGS(TableName, Operation, RecordID, Details)
-    VALUES ('MAINTENANCE', 'INSERT', :NEW.MaintenanceID,
-            'AvionID='||:NEW.AvionID||', Type='||:NEW.Typee||', Date='||TO_CHAR(:NEW.OperationDate,'YYYY-MM-DD'));
+    VALUES ('MAINTENANCE', 'INSERT', :NEW.maintenance_id,
+            'Avion_id='||:NEW.Avion_id||', Type='||:NEW.typee||', Date='||TO_CHAR(:NEW.OperationDate,'YYYY-MM-DD'));
 END;
 /
 
-CREATE OR REPLACE TRIGGER after_delete_maintenance
+
+CREATE OR REPLACE TRIGGER trg_after_delete_maintenance
 AFTER DELETE ON MAINTENANCE
 FOR EACH ROW
 DECLARE
     v_count NUMBER;
 BEGIN
+
     SELECT COUNT(*) INTO v_count
     FROM MAINTENANCE
-    WHERE AvionID = :OLD.AvionID AND State != 'Completed';
+    WHERE Avion_id = :OLD.Avion_id 
+      AND State != 'Completed';
 
+  
     IF v_count = 0 THEN
-        UPDATE AIRCRAFTS SET State = 'Ready' WHERE AvionID = :OLD.AvionID;
+        UPDATE Aircrafts 
+        SET State = 'Ready' 
+        WHERE Avion_id = :OLD.Avion_id;
     END IF;
 
+ 
     INSERT INTO LOGS(TableName, Operation, RecordID, Details)
-    VALUES ('MAINTENANCE', 'DELETE', :OLD.MaintenanceID,
-            'AvionID='||:OLD.AvionID||', Type='||:OLD.Typee||', Date='||TO_CHAR(:OLD.OperationDate,'YYYY-MM-DD'));
+    VALUES ('MAINTENANCE', 'DELETE', :OLD.maintenance_id,
+            'Avion_id='||:OLD.Avion_id||', Type='||:OLD.typee);
+END;
+/
+
+
+CREATE OR REPLACE TRIGGER trg_before_insert_maintenance
+BEFORE INSERT ON MAINTENANCE
+FOR EACH ROW
+DECLARE
+    v_avion_state VARCHAR2(50);
+BEGIN
+    SELECT State INTO v_avion_state FROM Aircrafts WHERE Avion_id = :NEW.Avion_id;
+    
+    IF v_avion_state = 'Hors Service' THEN
+        RAISE_APPLICATION_ERROR(-20002, 'Impossible de programmer une maintenance : l''avion est Hors Service.');
+    END IF;
 END;
 /
