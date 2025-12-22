3f57e88b2b6c727390d47dcec3a36fbfcb144b12
AsmaAoub <136008375+AsmaAoub@users.noreply.github.com>
2025-12-22 13:16:33 +0100

    Refactor triggers for reservations and logs

--- Aeoropor fastapi/plsql/triggers/triggers_reservations.sql
+++ Aeoropor fastapi/plsql/triggers/triggers_reservations.sql
@@ -4,7 +4,6 @@ FOR EACH ROW
 DECLARE
     v_taken NUMBER;
 BEGIN
-    
     SELECT COUNT(*) INTO v_taken
     FROM RESERVATIONS
     WHERE vol_num = :NEW.vol_num AND SeatCode = :NEW.SeatCode;
@@ -15,20 +14,19 @@ BEGIN
 END;
 /
 
-
 CREATE OR REPLACE TRIGGER after_insert_reservation
 AFTER INSERT ON RESERVATIONS
 FOR EACH ROW
 BEGIN
-   
+
     UPDATE FLIGHTS
     SET CurrentCapacity = CurrentCapacity + 1
     WHERE vol_num = :NEW.vol_num;
 
     
     INSERT INTO LOGS(TableName, Operation, RecordID, Details)
-    VALUES ('RESERVATIONS', 'INSERT', :NEW.reservation_id,
-            'Passager_id='||:NEW.Passenger_id||', vol_num='||:NEW.vol_num||', Seat='||:NEW.SeatCode);
+    VALUES ('RESERVATIONS', 'INSERT', :NEW.reservation_id, 
+            'Passager_id='||:NEW.Passenger_id||', Vol='||:NEW.vol_num);
 END;
 /
 
@@ -37,14 +35,12 @@ CREATE OR REPLACE TRIGGER after_delete_reservation
 AFTER DELETE ON RESERVATIONS
 FOR EACH ROW
 BEGIN
- 
     UPDATE FLIGHTS
     SET CurrentCapacity = CurrentCapacity - 1
     WHERE vol_num = :OLD.vol_num AND CurrentCapacity > 0;
 
- 
     INSERT INTO LOGS(TableName, Operation, RecordID, Details)
-    VALUES ('RESERVATIONS', 'DELETE', :OLD.reservation_id,
-            'Passager_id='||:OLD.Passenger_id||', vol_num='||:OLD.vol_num||', Seat='||:OLD.SeatCode);
+    VALUES ('RESERVATIONS', 'DELETE', :OLD.reservation_id, 
+            'Passager_id='||:OLD.Passenger_id||', Vol='||:OLD.vol_num);
 END;
 /
