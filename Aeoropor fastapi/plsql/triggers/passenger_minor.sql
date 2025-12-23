d0a27c5cc5eb234628e99c3313370f1e346dba4d
Saadiaboussiar <boussiarsaadia@gmail.com>
2025-12-23 17:36:15 +0100

    replaced and deleted some files

--- Aeoropor fastapi/plsql/triggers/passenger_minor.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE TRIGGER trg_passengers_guardian
-BEFORE INSERT OR UPDATE ON PASSENGERS
-FOR EACH ROW
-BEGIN
-    IF :NEW.Age_pass < 18 AND :NEW.Guardian_id IS NULL THEN
-        RAISE_APPLICATION_ERROR(-20001, 'Mineur doit avoir un Guardian.');
-    END IF;
-END;
-/
